bed3d459729f88ce90fd3128b87ffd6b6ff6fa40
Rishi <suryawanshirishi.nmims@gmail.com>
2020-08-01 18:11:46 +0530

    ReactJS8

--- src/index.js
+++ src/index.js
@@ -1,17 +1,104 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class OrderComponent extends React.Component
+{
+  constructor(props){
+    super(props);
+    this.state={quantity:'',address:''};
+  }
+  orderInfoChanged=(val)=>{
+    this.setState({quantity:val});
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  addressChanged=(val)=>{
+    this.setState({address:val});
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Welcome to Product Order Screen</h1>
+        <OrderInfoComponent quantity={this.state.quantity} onQuantityChange={this.orderInfoChanged}></OrderInfoComponent>
+        <AddressComponent address={this.state.address} onAddressChange={this.addressChanged}></AddressComponent>
+        <SummaryComponent quantity={this.state.quantity} address={this.state.address} onQuantityChange={this.orderInfoChanged}></SummaryComponent>
+
+      </div>
+    )
+  }
+
+}
+
+class OrderInfoComponent extends React.Component
+{
+  constructor(props){
+    super(props);
+  }
+  handleChange=e=>{
+    this.props.onQuantityChange(e.target.value);
+  }
+
+  render(){
+    return(
+      <div style={{border: '3px solid red'}}>
+        <h2>
+          Order Information
+        </h2>
+        <p><label>Product Name: <select>
+          <option value="Product-1">Product-1</option>
+          <option value="Product-2">Product-2</option>
+          <option value="Product-3">Product-3</option>
+        </select></label></p>
+        <p><label>Enter Quantity: <input type="text" value={this.props.quantity} onChange={this.handleChange}></input></label></p>
+      </div>
+    );
+  }
+}
+
+class AddressComponent extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  handleChange=e=>{
+    this.props.onAddressChange(e.target.value);
+  };
+  render(){
+    return(
+          <div style={{border:'3px solid red'}}>
+            <h2>Address Component</h2>
+            <p><label>Address: 
+              <textarea value={this.props.address} onChange={this.handleChange}></textarea></label></p>
+          </div>     
+    );
+  }
+}
+
+class SummaryComponent extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  handleChange=(e)=>{
+    this.props.onQuantityChange(e.target.value);
+  }
+  render(){
+    return(
+      <div style={{border:'3px solid red'}}>
+        <h2>Summary...</h2>
+          <p>
+            <label>Product Name : <b>Product - 1</b></label>
+          </p>
+          <p>
+            <label>Product Quantity : <input type="text" value={this.props.quantity} onChange={this.handleChange}></input></label>
+          </p>
+          <p>
+            <label>Address : <b>{this.props.address}</b></label>
+          </p>
+          <button>Place Order</button>
+      </div>
+    );
+  }
+}
+
+const element=<OrderComponent></OrderComponent>
+
+ReactDOM.render(element,document.getElementById("root"));
